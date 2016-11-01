ea847575af357f4bc8c9d26b54d07f4d1ca539c3
Marta Radeva <marta.radeva@code-runners.com>
2016-11-01 22:24:48 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :devices, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "devices#index"
+  root to: redirect('/devices')
 end
