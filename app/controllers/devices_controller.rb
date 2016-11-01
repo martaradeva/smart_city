e6f3bb7e41876f21c47ae4136c5e995955cfcc7f
Marta Radeva <marta.radeva@code-runners.com>
2016-11-01 22:56:27 +0200

    Add a device controller and test it in views

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,7 +1,18 @@
 class DevicesController < ApplicationController
   def index
+    set_devices
   end
 
   def show
+    set_device
+  end
+
+  private
+  def set_devices
+    @devices = Device.all
+  end
+
+  def set_device
+    @device = Device.find_by(id: params[:id])
   end
 end
