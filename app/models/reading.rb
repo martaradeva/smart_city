25ffd440847ec21e172b3d655823ac03d716c937
Marta Radeva <marta.radeva@code-runners.com>
2016-11-30 11:25:58 +0200

    Wip calibration gas sensors

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,7 +3,19 @@ class Reading < ApplicationRecord
   validates :device, presence: true
 
   def soil_hum_percent
-    per = self.hums / 11
+    per = self.hums * 100 / 1023
     per.round(2)
+  end
+
+  def co2_from_mq135
+    # rs = mq135
+    # ro = 41763 #from sensor calibration
+    # ppm = 116.6020682*((rs/ro)**(-2.769034857))
+    # ppm.round(2)
+    mq135
+  end
+
+  def lpg_from_mq5
+    mq5
   end
 end
